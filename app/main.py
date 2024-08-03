389fbdf982e4fce9389b85d114ef661a0149c9f4
tanism312 <165148186+tanism312@users.noreply.github.com>
2024-08-03 18:06:47 +1000

    frond end development

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    print("Abhishek")
-    return {"message": "Hello Test"}
+@app.get("/", response_class=HTMLResponse)
+async def read_home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/safety_insights", response_class=HTMLResponse)
+async def safety_insights(request: Request):
+    return templates.TemplateResponse("safety_insights.html", {"request": request})
