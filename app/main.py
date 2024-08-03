ebb1cbeb3e596df0f25fd04ff5a687eef70480fa
Abhishek Holla <abhishek.holla98@gmail.com>
2024-08-03 23:38:31 +1000

    Implemented folder structure and database mapping with fast API ORM sqlalchemy (#3)
    * Added basic folder structure for bikesafe navigator project
    
    * Updated the folder structure and created the database class for connecting to the database in an object oriented manner
    
    * frond end development
    
    * updated folder structure as per fast API standards
    
    * Revert "frond end development"
    
    This reverts commit 389fbdf982e4fce9389b85d114ef661a0149c9f4.
    
    * Implemented the dependency function of session db connection
    
    * Created the user API structure as buisness logic, repository layer etc
    
    * Created the database session and connection format
    
    ---------
    
    Co-authored-by: tanism312 <165148186+tanism312@users.noreply.github.com>

---
+++ app/main.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .controllers import user_controller
+from .db.models import models
+from . import schemas
+from .db.database import engine
+from dotenv import load_dotenv
+from .controllers import user_controller
+
+load_dotenv()
+models.Base.metadata.create_all(bind= engine)
+
+app = FastAPI()
+
+app.include_router(user_controller.router)
+
+
+# @app.post("/users/",response_model=schemas.User)
+# def post_user(user:schemas.UserCreate, db:Session=Depends(get_db)):
+#     db_user = user_controller.get_user_by_email(db, email=user.email)
+#     if db_user:
+#         raise HTTPException(status_code=400, detail="Email already registered")
+#     return user_controller.create_user(db=db,user=user)
+
+
+# @app.get("/users/", response_model=list[schemas.User])
+# def get_users(skip:int=0, limit:int=0, db:Session=Depends(get_db)):
+#     users = user_controller.get_users(db,skip=skip,limit=limit)
+#     return users
+
+
+# @app.get("/users/{user_id}/",response_model=schemas.User)
+# def get_user(user_id:int, db:Session=Depends(get_db)):
+#     db_user = user_controller.get_user(db,user_id =user_id )
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
+
+
+# @app.post("/users/{user_id}/todos/",response_model=schemas.Todo)
+# def post_todo_for_user(user_id:int, todo:schemas.TodoCreate, db:Session=Depends(get_db)):
+#     return user_controller.create_user_todo(db=db,user_id=user_id, todo=todo)
+
+
+# @app.get("/todos/", response_model=list[schemas.Todo])
+# def get_todos(skip:int=0,limit:int=100,db:Session=Depends(get_db)):
+#     todos = user_controller.get_todos(db,skip=skip,limit=limit)
+#     return todos
