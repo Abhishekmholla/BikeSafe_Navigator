a9a17bceec3c1edcd2763ab7d9a6909671002914
Abhishekmholla <abhishek.holla98@gmail.com>
2024-08-07 17:57:12 +1000

    Created the logic for interconnected tables with proper references

--- src/final_setup/model/models.py
+++ src/final_setup/model/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, ForeignKeyConstraint, Integer, String
 from ..db.database import Base
 from sqlalchemy.orm import relationship
 
@@ -24,7 +24,7 @@ class Accident(Base):
     accident_no = Column(String(255), primary_key=True, index=True)
     lga_name = Column(String(255), index=True)
     latitude = Column(String(255))
-    longitude = Column(Integer, ForeignKey("users.id"))
+    longitude = Column(String(255))
     severity = Column(String(255))
     nodes = relationship("Node",back_populates="accident")
     accident_route = relationship("Accident_Routes",back_populates="accident_route")
@@ -34,8 +34,8 @@ class Node(Base):
     node_id = Column(String(255), primary_key=True, index=True)
     lga_name = Column(String(255), index=True)
     latitude = Column(String(255))
-    longitude = Column(Integer, ForeignKey("users.id"))
-    accident_no = Column(String(255), index=True)
+    longitude = Column(String(255))
+    accident_no = Column(String(255), ForeignKey("accident.accident_no"))
     accident = relationship("Accident",back_populates="nodes")
     
 class Route(Base):
@@ -47,7 +47,9 @@ class Route(Base):
     
 class Accident_Route(Base):
     __tablename__ = "accident_route"
-    accident_no =  Column(String(255), primary_key=True, index=True)
-    route_id = Column(Integer, primary_key=True, index=True)
+    accident_route_id = Column(Integer, primary_key=True, autoincrement=True)
+    accident_no =  Column(String(255), ForeignKey("accident.accident_no"), index=True)
+    route_id = Column(Integer,ForeignKey("route.route_id"), index=True)
     accident_route = relationship("Accident",back_populates="accident_route")
     route = relationship("Route",back_populates="accident_routes")
+    
